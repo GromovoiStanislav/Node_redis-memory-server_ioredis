c9fc7c2612674833fe53478a414af0c0d204728f
Gromovoi Stanislav <gromila12@rambler.ru>
2022-11-02 10:42:31 +0600

    commit

--- index.mjs
+++ index.mjs
@@ -9,23 +9,23 @@ const redis = new Redis({
   port,
 });
 
-// redis.set('mykey', 'value');
+redis.set('mykey1', 'value');
 
-// redis.get('mykey', (err, result) => {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log(result); // Prints "value"
-//   }
-// });
+redis.get('mykey1', (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(result); // Prints "value"
+  }
+});
 
-// redis.get('mykey').then((result) => {
-//   console.log(result); // Prints "value"
-// });
+redis.get('mykey1').then((result) => {
+  console.log(result); // Prints "value"
+});
 
-redis.set('mykey', 'hello', 'EX', 10);
+redis.set('mykey2', 'hello', 'EX', 10);
 setTimeout(() => {
-  redis.get('mykey').then((result) => {
+  redis.get('mykey2').then((result) => {
     console.log(result); // Prints "value"
   });
 }, 15 * 1000);
